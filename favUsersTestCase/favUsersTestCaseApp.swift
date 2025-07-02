18e840e22291592cae6e377f74aa66e3c19a698e
onuraltintas2045 <onur.altintas2045@gmail.com>
2025-07-03 00:20:27 +0300

    Initial Commit

---
+++ favUsersTestCase/favUsersTestCaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  favUsersTestCaseApp.swift
+//  favUsersTestCase
+//
+//  Created by Onur Altintas on 2.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct favUsersTestCaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
